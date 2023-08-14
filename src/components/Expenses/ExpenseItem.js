82fdade15a119a4c18d301c6572bad3b9b91eaa0
priyankasewaiwar <priynkadohare@gmail.com>
2023-08-14 23:09:21 +0530

    Button and Event Listner

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,10 +4,15 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+    const expenseHandler = () =>{
+
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate expense={props.expense}/>
             <ExpenseDetail expense={props.expense}/>
+            <button onClick={expenseHandler}>Change Title</button>
+            <button onClick={expenseHandler}>Delete Expense</button>
         </Card>
         );
 }
