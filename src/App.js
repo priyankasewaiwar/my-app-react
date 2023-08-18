d1b5ce7cc2af02844153f2b3904e300e918d654b
priyankasewaiwar <priynkadohare@gmail.com>
2023-08-18 23:32:49 +0530

    use State function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Expenses from './components/Expenses/Expenses';
 const App = () => {
   const expenses = [
     { id: 'e1', title: 'Food', location: 'New Delhi', amount: 10, date: new Date(2023, 6, 28) },
-    { id: 'e1', title: 'Food', location: 'New Delhi', amount: 100, date: new Date(2023, 6, 29) }
+    { id: 'e1', title: 'Cosmetics', location: 'New Delhi', amount: 100, date: new Date(2023, 6, 29) }
   ]
   return (
     <div>
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,32 @@
+import React, {useState} from 'react';
 import './ExpenseItem.css';
-import ExpenseDetail from './ExpenseDetail';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-    const expenseHandler = () =>{
 
+    const [title, setTitle] = useState(props.expense.title);
+    const [amount, setAmount] = useState(props.expense.amount);
+
+    const titleHandler = () =>{
+        setTitle(title+' Updated !')
+        console.log(title);
     }
+
+    const expenseHandler = () =>{
+        setAmount("100");
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate expense={props.expense}/>
-            <ExpenseDetail expense={props.expense}/>
-            <button onClick={expenseHandler}>Change Title</button>
-            <button onClick={expenseHandler}>Delete Expense</button>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__locationOfExpenditure'>{props.expense.location}</div>
+                <div className='expense-item__price'>Rs {amount}</div>
+            </div>
+            <button onClick={titleHandler}>Change Title</button>
+            <button onClick={expenseHandler}>Change Expense</button>
         </Card>
         );
 }
