6a639a5f3f20668c7a4956fdfc436c8df37c9a2f
priyankasewaiwar <priynkadohare@gmail.com>
2023-10-19 00:23:09 +0530

    Conditional Rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,34 @@
+import React, {useState} from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 
 const Expenses = (props) => {  
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear =>{
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.items.filter(expense =>{
+        return expense.date.getFullYear().toString()===filteredYear;
+    });
+
+    let expensesContent = <h2>No Expenses Found </h2>;
+
+    if(filteredExpenses.length === 1){
+        expensesContent = filteredExpenses.map(expense=> <div> <ExpenseItem key={expense.id} expense={expense }/><p>Only single Expense here. Please add more...</p></div>);
+    }
+
+    if(filteredExpenses.length > 1){
+        expensesContent = filteredExpenses.map(expense=>  <ExpenseItem key={expense.id} expense={expense }/>);
+    }
+
     return (
         <Card className='expenses'>
-            {props.items.map(expense=>  <ExpenseItem key={expense.id} expense={expense }/>)}
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/> 
+            {expensesContent}
         </Card>
     );
 }
